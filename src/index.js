b2d82767ff61793cd6b815f310984d24f64381c1
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-03-26 17:04:38 +0000

    merge Bib.js into index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Bib from './Bib.js';
+import Bibparser from './bibparser.jison';
 import logo from './doi2bib-logo.png';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -29,6 +29,95 @@ const arxivid2doi = (arxivid) => fetch('https://arxiv.org/bibtex/' + arxivid).th
   return response.text()
 })
 
+const SPECIAL_CHARS = {
+  'à': '\\`a',
+  'ô': '\\^o',
+  'ê': '\\^e',
+  'â': '\\^a',
+  '®': '{\\textregistered}',
+  'ç': '\\c{c}',
+  'ö': '\\"{o}',
+  'ä': '\\"{a}',
+  'ü': '\\"{u}',
+  'Ö': '\\"{O}',
+  'Ä': '\\"{A}',
+  'Ü': '\\"{U}'
+};
+
+/**
+ * Sometimes, the greek chars aren't properlty formatted in the received bib.
+ * e.g. the bib for 10.1002/cncr.29046 contains {\varEpsilon} instead of {$\varEpsilon$}.
+ * There, we inject the missing $ chars into the title string.
+ */
+const insertDollars = (str) => str.replace(/(\{)(\\var[A-Z]?[a-z]*)(\})/, '$1$$$2$$$3');
+
+const encodeSpecialChars = (str) => str.replace(
+  new RegExp(Object.keys(SPECIAL_CHARS).join('|'),'gi'),
+  (matched) => SPECIAL_CHARS[matched]
+);
+
+export default class Bib {
+  constructor(bibStr) {
+    this.bib = Bibparser.parse(bibStr);
+
+    if (this.bib.tags.pages === 'n/a-n/a') {
+      delete this.bib.tags.pages;
+    }
+    if (this.bib.tags.pages && this.bib.tags.pages.indexOf('--') === -1) {
+      this.bib.tags.pages = this.bib.tags.pages.replace(/-/g, '--');
+    }
+
+    /* id specific */
+    if (this.bib.id) {
+      this.bib.id = this.bib.id.replace(/_/g, '');
+    }
+
+    // bib url contains url encoding -> we decode those characters here
+    if (this.bib.tags.url) {
+      this.bib.tags.url = decodeURIComponent(this.bib.tags.url);
+    }
+
+
+    if (this.bib.tags.title) {
+      if (Array.isArray(this.bib.tags.title)) {
+        this.bib.tags.title = this.bib.tags.title.map(t => insertDollars(t))
+      } else {
+        this.bib.tags.title = insertDollars(this.bib.tags.title);
+      }
+    }
+
+    // remove brackets from month
+    console.log(bibStr);
+    console.log(this.bib.tags.month);
+  }
+
+  toPrettyString() {
+    var result;
+
+    result = '@' + this.bib.type + '{' + this.bib.id;
+
+    Object.keys(this.bib.tags).forEach(key => {
+      const useBrackets = !['month'].includes(key);
+      const value = this.bib.tags[key];
+      result += ',\n  ' + key + ' = ';
+      if (useBrackets) {
+        result += '{';
+      }
+      result += encodeSpecialChars(value.join ? value.join(', ') : value);
+      if (useBrackets) {
+        result += '}';
+      }
+    });
+
+    result += '\n}';
+
+    return result;
+  };
+
+  getURL() {
+    return this.bib.tags.url;
+  }
+}
 
 class Code extends Component {
   render() {
