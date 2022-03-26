0afb4dd048bba89bd7bc39ea4cceaa1e71ee6a1e
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2022-03-26 12:07:30 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import About from './components/About';
-import Help from './components/Help';
-import Terms from './components/Terms';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Doi2Bib from './components/Doi2Bib';
 
 class App extends Component {
@@ -13,30 +10,10 @@ class App extends Component {
         <div>
           <div className="container">
             <Switch>
-              <Route path="/about" component={About} />
-              <Route path="/terms" component={Terms} />
-              <Route path="/help" component={Help} />
               <Route path="/bib/*" component={Doi2Bib} />
               <Route path="*" component={Doi2Bib} />
             </Switch>
           </div>
-          <footer className="text-center">
-            &copy; 2017 doi2bib
-            &nbsp;&nbsp;
-            <Link to="/"><i className="fa fa-home"></i></Link>
-            &nbsp;&nbsp;
-            <a href="https://twitter.com/doi2bib" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-lg"></i></a>
-            &nbsp;&nbsp;
-            <a href="https://github.com/davidagraf/doi2bib2" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-lg"></i></a>
-            &nbsp;&nbsp;
-            <Link to="/about">About</Link>
-            &nbsp;&nbsp;
-            <Link to="/about?donate=true">Donate</Link>
-            &nbsp;&nbsp;
-            <Link to="/help">Help</Link>
-            &nbsp;&nbsp;
-            <Link to="/terms">Terms</Link>
-          </footer>
         </div>
       </BrowserRouter>
     );
