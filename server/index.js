cd790779457df13729255bca03a58e0c4458cded
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2022-03-26 15:55:29 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,37 +1,34 @@
- 'use strict';
+'use strict';
+
+const doi2bib = (doi) => fetch('https://doi.org/' + doi, {headers: {'Accept': 'application/x-bibtex; charset=utf-8'}}).then(response => {
+  if (!response.ok) throw new Error(response.statusCode);
+  return response.text()
+});
+
+const pmid2doi = (pmid) => fetch('https://www.pubmedcentral.nih.gov/utils/idconv/v1.0/?format=json&ids=' + pmid).then(response => {
+  if (!response.ok) throw new Error(response.statusCode);
+  return response.json()
+}).then(body => {
+  if (!body.records || !body.records[0]) throw new Error(204); // not found
+  return doi2bib(body.records[0].doi)
+});
+
+const arxivid2doi = (arxivid) => fetch('https://arxiv.org/bibtex/' + arxivid).then(response => {
+  if (!response.ok) throw new Error(response.statusCode);
+  return response.text()
+})
 
 const http = require('http'),
     express = require('express'),
-    cors = require('cors'),
     app = express(),
-    env = process.env.NODE_ENV || 'development',
     port = process.env.PORT || 3001,
     path = require('path'),
-    doi2bib = require('./doi2bib'),
-    helmet = require('helmet'),
-    rateLimit = require("express-rate-limit"),
-    morgan = require('morgan');
 
-app.use(morgan('short'));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
 
-  //  apply to all requests
-app.use(limiter);
-app.use(helmet());
-
-if ('production' === env) {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-} else {
-  app.use(cors());
-}
-
-var
-genErrorHandler = function(res) {
-  return function(errorCode) {
+const genErrorHandler = (res) => {
+  return (errorCode) => {
     if (http.STATUS_CODES[errorCode]) {
       res.writeHead(errorCode);
       res.end(http.STATUS_CODES[errorCode]);
@@ -40,58 +37,40 @@ genErrorHandler = function(res) {
       res.end(http.STATUS_CODES[500]);
     }
   };
-},
-genSuccessHandler = function(res) {
-  return function(bib) {
-    res.end(bib);
-  };
-};
+}
 
-app.get('/2/doi2bib', function(req, res) {
+app.get('/2/doi2bib', (req, res) => {
   res.set('Content-Type', 'application/x-bibtex');
-
   if (!/^10\..+\/.+$/.test(req.query.id)) {
     res.writeHead(400);
     res.end('Invalid DOI');
   } else {
-    doi2bib.doi2bib(req.query.id).then(genSuccessHandler(res), genErrorHandler(res));
+    doi2bib(req.query.id).then(res.end, genErrorHandler(res));
   }
 });
 
-app.get('/2/pmid2bib', function(req, res) {
+app.get('/2/pmid2bib', (req, res) => {
   res.set('Content-Type', 'application/x-bibtex');
-
-
   if (!/^\d+$|^PMC\d+(\.\d+)?$/.test(req.query.id)) {
     res.writeHead(400);
     res.end('Invalid PMID');
   } else {
-    doi2bib.pmid2doi(req.query.id).
-      then(function(doi) {
-        return doi2bib.doi2bib(doi);
-      }).
-      then(genSuccessHandler(res), genErrorHandler(res));
+    pmid2doi(req.query.id).then(res.end, genErrorHandler(res));
   }
 });
 
-app.get('/2/arxivid2bib', function(req, res) {
+app.get('/2/arxivid2bib', (req, res) => {
   res.set('Content-Type', 'application/x-bibtex');
   if (!/^\d+\.\d+(v(\d+))?$/.test(req.query.id)) {
     res.writeHead(400);
     res.end('Invalid arXiv ID');
   } else {
-    doi2bib.arxivid2doi(req.query.id).
-      then(function(doi) {
-        return doi2bib.doi2bib(doi);
-      }).
-      then(genSuccessHandler(res), genErrorHandler(res));
+    arxivid2doi(req.query.id).then(res.end, genErrorHandler(res));
   }
 });
 
 app.listen(
   port,
   '127.0.0.1',
-  function() {
-    console.log('node server started');
-  }
+  () => {console.log('node server started')},
 );
